cb827a70d45d3d39f672fa73a94d08fa756698a0
akitodr <amarinadelara@gmail.com>
2022-08-08 22:52:05 -0300

    Alteração de conversão de String

--- Exercicio02.py
+++ Exercicio02.py
@@ -1,6 +1,6 @@
 carroValor = 100.0
 dias = int(input("Por quantos dias o carro será alugado? "))
 
-valorTotal = str(carroValor * dias)
+valorTotal = carroValor * dias
 
-print("O valor total é de: R$ " + valorTotal)
+print("O valor total é de: R$ " + str(valorTotal))
