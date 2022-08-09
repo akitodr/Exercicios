f463b6c9e5c9ea0576bbdb8d698f07377b5ef1fc
akitodr <amarinadelara@gmail.com>
2022-08-08 22:48:04 -0300

    Resolução de Exercícios

---
+++ Exercicio01.py
@@ -0,0 +1,7 @@
+anoNascimento = int(input("Digite o ano do seu nascimento: "))
+
+ano = 2022
+idade = ano - anoNascimento
+
+print("A sua idade é de: " + str(idade))
+print("A sua idade é de: ", idade, "anos")
---
+++ Exercicio02.py
@@ -0,0 +1,6 @@
+carroValor = 100.0
+dias = int(input("Por quantos dias o carro será alugado? "))
+
+valorTotal = str(carroValor * dias)
+
+print("O valor total é de: R$ " + valorTotal)
